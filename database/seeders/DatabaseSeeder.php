5a6f14c293a1c20134d7d2341e45aee061b8f9ab
darcioSoares <darcio80@gmail.com>
2025-02-09 15:32:05 -0300

    Criando Usuario com seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             MedicoSeeder::class,
             PacienteSeeder::class,
             ConsultaSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
