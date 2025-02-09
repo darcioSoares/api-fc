495ac241642214f97def9731491a0c13b41e8b25
darcioSoares <darcio80@gmail.com>
2025-02-09 17:49:54 -0300

    Criando Validação para não deixar agendar, caso já tenha um agendamento para aquele medico naquele horario

--- app/Http/Controllers/ConsultaController.php
+++ app/Http/Controllers/ConsultaController.php
@@ -12,25 +12,33 @@ class ConsultaController extends Controller
 {
     public function agendar(Request $request)
     {
-        // Validação dos dados
         $validator = Validator::make($request->all(), [
             'medico_id' => 'required|exists:medicos,id',
             'paciente_id' => 'required|exists:pacientes,id',
             'data' => 'required|date_format:Y-m-d H:i:s|after:now',
         ]);
-
+    
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
-        }
+        }    
 
-        // Criar a consulta
+        $consultaExistente = Consulta::where('medico_id', $request->medico_id)
+            ->where('data', $request->data)
+            ->exists();
+    
+        if ($consultaExistente) {
+            return response()->json([
+                'error' => 'O médico já tem uma consulta agendada nesse horário.'
+            ], 409); 
+        }
+    
         $consulta = Consulta::create([
             'medico_id' => $request->medico_id,
             'paciente_id' => $request->paciente_id,
-            'cidade_id' => Medico::find($request->medico_id)->cidade_id, // Pegando a cidade do médico
+            'cidade_id' => Medico::find($request->medico_id)->cidade_id,
             'data' => $request->data,
         ]);
-
+    
         return response()->json($consulta, 201);
    }
 }
